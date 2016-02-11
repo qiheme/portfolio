d02ba4ff0c6ecafeb2f678ef7661061ee9cb86b4
Quincy Iheme <qiheme@gmail.com>
2016-02-11 07:24:30 -0500

    Add Bootstrap Creative theme

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'font-awesome-sass'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.easing.min
+//= require jquery.fittext
+//= require bootstrap
+//= require bootstrap.min
+//= require classie
+//= require cbpAnimatedHeader
+//= require wow.min
+//= require creative
 //= require turbolinks
 //= require_tree .
