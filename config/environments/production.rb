0086499046c9cc16ff74ac087c6aa82cb05524c6
Quincy Iheme <qiheme@gmail.com>
2016-02-15 18:37:46 -0500

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,6 @@ Quincy::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( jquery )
   config.assets.precompile += %w( bootstrap.min )
   config.assets.precompile += %w( jquery.easing.min )
