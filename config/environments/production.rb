b1036bcb5c029289e8367fdcd80b1888447ef049
Quincy Iheme <qiheme@gmail.com>
2016-02-15 19:30:42 -0500

    Add ALL precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,6 @@ Quincy::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -59,12 +58,7 @@ Quincy::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( jquery )
-  config.assets.precompile += %w( bootstrap.min )
-  config.assets.precompile += %w( jquery.easing.min )
-  config.assets.precompile += %w( jquery.fittext )
-  config.assets.precompile += %w( wow.min )
-  config.assets.precompile += %w( creative )
+  config.serve_static_files = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
---
+++ config/initializers/assets.rb
@@ -0,0 +1,8 @@
+Rails.application.config.assets.precompile += [
+'jquery.js',
+'bootstrap.min.js',
+'jquery.easing.min.js',
+'jquery.fittext.js',
+'wow.min.js',
+'creative.js'
+]
