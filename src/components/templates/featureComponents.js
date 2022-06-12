87bfb49ac456ed6f8b6a6016a681b43134eca84e
Quincy Iheme <qiheme@gmail.com>
2022-06-12 07:20:00 -0400

    Change two cards on features tile

--- src/components/templates/featureComponents.js
+++ src/components/templates/featureComponents.js
@@ -1,6 +1,8 @@
 import GithubIconImage from "vendor/feather-icons/github.svg";
 import BriefcaseIcon from "vendor/feather-icons/briefcase.svg";
 import PersonIcon from "vendor/feather-icons/user.svg";
+import MicIcon from "vendor/feather-icons/mic.svg";
+import Lightning from "vendor/feather-icons/zap.svg";
 
 export const featureCards = [
   {
@@ -22,15 +24,15 @@ export const featureCards = [
       "Volunteering at various organizations devoted towards improving equity within the technology industry",
   },
   {
-    imageSrc: PersonIcon,
+    imageSrc: MicIcon,
     title: "Conference Speaker",
     description:
       "Speaking at conferences with a focus on the technology industry and people leadership",
   },
   {
-    imageSrc: PersonIcon,
+    imageSrc: Lightning,
     title: "Freelance Developer",
     description:
-      "Supporting small businesses with aims to reach there goals of engaging their customers",
+      "Supporting small businesses who seek to digitally engage their customers with highly performant websites",
   },
 ];
