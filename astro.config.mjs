6677494a721e7af4896361693f4ef0410a9e9549
Quincy Iheme <qiheme@gmail.com>
2026-06-21 11:30:53 -0400

    Add Cobalt UI component library integration with React (#29)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
